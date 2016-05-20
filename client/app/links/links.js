a9300af241c5b7996fef4cefeac486c710c66576
aphavichitr <andrew.phavichitr@gmail.com>
2016-05-20 15:48:11 -0700

    Create links factory and links controller

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,26 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+
+  $scope.getAll = function() {
+    Links.getAll()
+      .then(function(links) {
+        $scope.data.links = links;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.addOne = function() {
+    Links.addOne($scope.link)
+      .then(function(data) {
+        $scope.data.links.push(data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 });
+
